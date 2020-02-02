d1696932be3266e9a049fdacaa9e77dae848108a
hwax0r <sergeevdavid38@gmail.com>
2020-02-02 20:21:26 +0300

    login and register now working

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# _still nothing interesting in here_
-1. rly
-* sorry
-
-| really | sorry |
-| ------------- |
-| nothing | interesting |
-
-
+# 02.02.2020
+* Now my plans are to make DataBase without sql/nosql databases. I will use txt files
+* I'd like to make a gui to this project using QT or another framework
+* Also i'd like to use web-server to remote view and editing of files to other employees. Though I would definitely have the necessity to change the auth system
+* every file wust be encoded with encryption system
+* every new employee must use the code given to them by administrator to register their account. It will make the system more secure 
+* cmd-line interface must be more user-friendly
--- main.py
+++ main.py
@@ -12,21 +12,23 @@ def cls():
 
     
 def signin_register_screen():
+    cls()
     print('Hi, pjct_Hospital employees')
     print('you have to 1:login or 2:register')
-    choose = int(input('1 or 2: '))
-    if choose == 1:
-        cls()
-        return signin()
-    elif choose == 2:
-        cls()
-        return register()
-    else:
-        print('Error')
-        print('there are no such option')
-        time.sleep(10)
-        cls()
-        return signin_register_screen
+    while True:
+        choose = input('1 or 2: ')
+        if choose == '1':
+            cls()
+            return signin()
+        elif choose == '2':
+            cls()
+            return register()
+        else:
+            print('Error')
+            print('there are no such option')
+            time.sleep(4)
+            cls()
+            return signin_register_screen
 
     
 def signin():
@@ -39,8 +41,9 @@ def signin():
         line = fin.readline()
         if string == line:
             cls()
-            return first_scene ###############################################################
-        if line == '' or ' ':
+            return first_scene 
+        elif line == '' or ' ':
+            cls()
             print('Error')
             print('There are no such login or password or even account')
             print('Please contact your system administrator')
@@ -50,11 +53,48 @@ def signin():
 
 
 def register():
-    fout = open('employees/accounts.txt','wt')
+    cls()
+    fout = open('employees/accounts.txt','a')
     print('This is Register screen \n')
     login = input('login: ')
     passwd = input('password: ')
-    fout.write()
+    if find_employee_account(login, passwd):
+        cls()
+        print('Error')
+        #print('This account already exists')
+        print('Please change your login and password')
+        time.sleep(2)
+        cls()
+        print('Do you want to \n')
+        print('1:move back to login/register screen \n')
+        print('or \n')
+        print('2:register one more time? \n')
+        while True:
+            user_input = input('1 or 2: ')
+            if user_input == '1':
+                return signin_register_screen()
+            elif user_input == '2':
+                return register()
+            else:
+                print('Error')
+                print('There are no such option')
+                time.sleep(1)
+    else:
+        fout.write(login + ' ' + passwd + '\n')
+
+def find_employee_account(login, passwd):
+    fin = open('employees/accounts.txt','rt')
+    string = login + ' ' + passwd
+    while True:
+        line = fin.readline()
+        if string == line:
+            return True
+        else:
+            return False
+
+
+
+register()
 
     
     
